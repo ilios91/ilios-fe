492a591a10a544c4f7c3cfa6a91743b0a3de44f5
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-21 15:58:22 +0000

    update service section

--- src/components/sections/Service.tsx
+++ src/components/sections/Service.tsx
@@ -16,10 +16,20 @@ export default function Service() {
 
         {/* Text content */}
         <div className="absolute left-0 top-0 w-full h-full z-40">
-          <div className="w-[90%] flex mx-auto bg-yellow-500 z-50 mt-[79px]">
+          <div className="w-[90%] flex mx-auto mt-[79px]">
             <div>
-              <h2 className="text-[32px] leading-[39.01px] font-bold text-white">For Suppliers</h2>
-              <p></p>
+              <h2 className="text-[32px] leading-[39.01px] font-bold text-white">
+                For Suppliers
+              </h2>
+              <p className="font-normal w-[358px] text-2xl leading-7 text-white mt-7">
+                Expand Your Reach :-List your products on a trusted platform
+                where healthcare providers shop for quality and reliability.
+                Manage inventory, track sales, and grow your business all in one
+                place
+              </p>
+              <button className="font-bold leading-[24.38px] text-[20px] w-[304px] h-16 rounded-lg bg-white text-blue-normal mt-[52px]">
+                Sign up as a Supplier
+              </button>
             </div>
           </div>
         </div>
@@ -36,6 +46,31 @@ export default function Service() {
         />
       </div>
       <div className="h-[665px] max-w-[100vw] relative">
+        {/* Opacity */}
+        <div className="absolute left-0 top-0 w-full h-full z-10">
+          <div className="max-w-[626px] bg-[#000000] opacity-60 h-full"></div>
+        </div>
+
+        {/* Text content */}
+        <div className="absolute left-0 top-0 w-full h-full z-40">
+          <div className="w-[90%] flex mx-auto mt-[79px]">
+            <div>
+              <h2 className="text-[32px] leading-[39.01px] font-bold text-white">
+                For Buyers
+              </h2>
+              <p className="font-normal w-[421px] text-2xl leading-7 text-white mt-7">
+                Streamline Your Procurement :-Access verified suppliers and
+                high-quality products at competitive prices. Simplify
+                procurement processes, manage orders, and ensure on-time
+                delivery for your healthcare needs
+              </p>
+              <button className="font-bold leading-[24.38px] text-[20px] w-[304px] h-16 rounded-lg bg-white text-blue-normal mt-[52px]">
+                Sign up as a Buyer
+              </button>
+            </div>
+          </div>
+        </div>
+
         <Image
           src={serviceImage2}
           alt="service image"
