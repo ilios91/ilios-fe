2e20fc8558068f8721caed254a70bd4f27512ef4
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-21 10:21:53 +0000

    minor fix

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,9 +1,22 @@
-import styles from '../styles/navbar.module.scss';
+import Link from "next/link";
+import styles from "../styles/navbar.module.scss";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <header className={`${styles.navbar} text-yellow-400`} >
-      Navbar
-    </header>
+    <nav className={`${styles.navbar} h-[122px] bg-white w-full`}>
+      <div className="w-[90%] mx-auto flex justify-between items-center h-full">
+        <div>
+          <Link href="/" className="block">
+            <Image
+              src="/logo.svg"
+              alt="Ilios"
+              className="h-[40px] w-[40px] mx-auto"
+            />
+          </Link>
+        </div>
+        <div></div>
+      </div>
+    </nav>
   );
 }
