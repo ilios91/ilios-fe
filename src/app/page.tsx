5fca8267abb90cb2bcc756dc7e99b220f07210db
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-23 19:44:02 +0000

    modified hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <main>
       <Hero/>
-      <About/>
-      <Service/>
-      <Faq/>
+      {/* <About/> */}
+      {/* <Service/> */}
+      {/* <Faq/> */}
     </main>
   );
 }
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -4,13 +4,14 @@ import HeroImage from "@/assets/images/hero.svg";
 
 export default function Hero() {
   return (
-    <section className={`relative w-full top-[90px] h-[calc(100vh-90px)]`}>
+    <section className={`relative w-[100vw] h-[calc(100vh-90px)] top-[90px] sm:top-[90px]`}>
       <Image
         fill={true}
         src={HeroImage}
         alt="hero"
         objectFit="cover"
         priority
+        sizes="100vw"
       />
 
       {/* Overlay */}
@@ -20,20 +21,15 @@ export default function Hero() {
 
       <div className="text-white absolute inset-0 max-w-[1192px] mx-auto flex flex-col justify-center items-center text-center">
         {/* ... content ... */}
-        <h1 className="font-bold align-center max-xs:leading-[40px] w-[90%] text-2xl sm:text-4xl md:w-full md:text-5xl md:leading-[58px]">
+        <h1 className="font-bold align-center max-xs:leading-[40px] w-[90%] text-[26px] sm:text-4xl md:w-full md:text-5xl md:leading-[58px]">
           Simplifying Healthcare Procurement for Facilities and Suppliers
         </h1>
-        <p className="font-normal text-[18px] mt-6 max-w-[1022px]">
-          To be proactive in providing a stable framework for the economic
-          development of Nigeria, through effective, efficient, and transparent
-          implementation of monetary and exchange rate policy, and management of
-          the financial sector
+        <p className="font-normal text-sm sm:text-[18px] mt-6 w-[90%] lg:max-w-[1022px]">
+          To be proactive in providing a stable framework for the economic development of Nigeria, through effective, efficient, and transparent
+          implementation of monetary and exchange rate policy, and management of the financial sector
         </p>
-        <div className="mt-24">
-          <button className="w-[274px] h-16 text-blue-normal bg-white font-bold text-[20px] leading-6 rounded-lg">
-            {" "}
-            Get started
-          </button>
+        <div className="mt-20 md:mt-24">
+          <button className="w-[224px] sm:w-[274px] h-14 text-blue-normal bg-white font-bold text-base sm:text-[20px] leading-6 rounded-lg"> Get started </button>
         </div>
       </div>
     </section>
