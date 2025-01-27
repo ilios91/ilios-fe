ce0e5d2477341ca89c863c383368f303902d0355
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-27 23:18:04 +0000

    minor fix

--- src/components/sections/Faq.tsx
+++ src/components/sections/Faq.tsx
@@ -3,20 +3,19 @@ import Accordion from "./Accordion";
 export default function Faq() {
   return (
     <section className="relative w-full h-[502px] bg-[#EDF4FF]">
-      <div className="pt-24 w-[90%] mx-auto flex flex-col md:flex-row items-center justify-between bg-yellow-400">
-        <div className="basis-1/2">
-          <h6 className="font-medium text-2xl leading-[29.69px] text-main-black">
-            FAQ Section
-          </h6>
-          <h4 className="font-bold text-2xl leading-[29.69px] text-main-black">
-            {"Got Questions? We've Got Answers"}
-          </h4>
+      <div className="pt-7 w-[90%] mx-auto flex flex-col gap-y-5 || md:flex-row md:items-center md:justify-between md:pt-24">
+        
+        <div className="basis-1/2 font-bold text-[#171717] || md:text-main-black">
+          <h6 className="text-xl leading-[29.69px] || md:text-2xl"> FAQ Section </h6>
+          <h4 className="font-bold text-base leading-[29.69px] || md:text-2xl"> {"Got Questions? We've Got Answers"} </h4>
         </div>
-        <div className="basis-1/2">
-          <h4 className="font-medium text-2xl leading-[29.69px] text-main-black"> Frequently Ask FAQs: </h4>
+
+
+        <div className="basis-1/2 text-[#171717] md:text-main-black">
+          <h4 className="font-semibold text-center text-base leading-[29.69px] || md:text-2xl "> Frequently Ask FAQs: </h4>
 
           {/* Accordions */}
-          <div className="mt-9">
+          <div className="mt-6 || md:mt-9">
             <Accordion text="How does onboarding work for suppliers and buyers?"/>
             <Accordion text="What measures are in place to ensure product quality?"/>
             <Accordion text="Can I track my orders on the platform?" />
