22faa947b96a16a6d8ecf7c18b2790f91c3f0a7f
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-28 10:18:16 +0000

    minor fix

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -2,26 +2,42 @@
 
 import Image from "next/image";
 import AboutImage from "@/assets/images/about.svg";
-import useWindowSize from "../hooks/useWindowSize";
+import AboutImageMobile from "@/assets/images/about-m.svg";import useWindowSize from "../hooks/useWindowSize";
 import { screenSizes } from "../utils/functions";
 
 export default function About() {
 const { screenWidth, screenHeight } = useWindowSize();
 
 const imageWidth = screenSizes("width", screenWidth, screenHeight);
-const imageHeight = screenSizes("height", screenWidth, screenHeight)
+const imageHeight = screenSizes("height", screenWidth, screenHeight);
+
+const width = () => {
+  if(screenWidth < 360 ) return 197
+    else if(screenWidth >= 360 && screenWidth < 480) return 297
+    else if(screenWidth >= 480 && screenWidth < 576) return 197
+    else if(screenWidth >= 576 && screenWidth < 768) return 197
+    else if(screenWidth >= 768 && screenWidth < 992) return 197
+    else if(screenWidth >= 992 && screenWidth < 1280) return 197
+    else if (screenWidth >= 1280 ) return 197  
+}
+
+console.log(imageWidth);
+console.log(imageHeight);
 
   
   return (
     <section id="about" className="w-[90%] mx-auto relative">
       <div className="flex flex-col md:flex-row items-center w-full gap-x-10 gap-y-10 mt-24 mb-8 h-full">
-        <div className="relative xs:w-[380px] xs:h-[380px] sm:w-[520px] sm:h-[520px] tablet:w-[600px] tablet:h-[600px] lg:w-[604px] lg:h-[627px]">
+        {/* <div className="relative xs:w-[300px] xs:h-[380px] sm:w-[520px] sm:h-[520px] tablet:w-[600px] tablet:h-[600px] lg:w-[604px] lg:h-[627px]"> */}
+        <div className="relative flex justify-center w-full sm:w-[520px] sm:h-[520px] tablet:w-[600px] tablet:h-[600px] lg:w-[604px] lg:h-[627px]">
           <Image 
-            src={AboutImage} 
+            src={screenWidth < 576 ? AboutImageMobile : AboutImage} 
             alt="about-image" 
-            fill={true}
+            // fill={true}
             className="object-cover"
             priority
+            width={imageWidth}
+            height={204}
           />
         </div>
         <div className="w-full md:w-1/2">
--- src/components/utils/functions.ts
+++ src/components/utils/functions.ts
@@ -4,24 +4,12 @@ type ScreenSize = "width" | "height"
 export const screenSizes = (type: ScreenSize, width = 0, height = 0 ) => {
 
   if(type === "width"){
-    return {
-      xs: width < 576,
-      sm: width >= 576 && width < 768,
-      md: width >= 768 && width < 992,
-      tablet: width >= 992 && width < 1280,
-      lg: width >= 1280 && width < 1440,
-      xl: width >= 1440
-    }
-  }
-
-  else if (type === "height"){
-    return {
-      xs: height < 576,
-      sm: height >= 576 && height < 768,
-      md: height >= 768 && height < 992,
-      tablet: height >= 992 && height < 1280,
-      lg: height >= 1280 && height < 1440,
-      xl: height >= 1440
-    }
+    if(width < 360 ) return 197
+    else if(width >= 360 && width < 480) return 297
+    else if(width >= 480 && width < 576) return 197
+    else if(width >= 576 && width < 768) return 197
+    else if(width >= 768 && width < 992) return 197
+    else if(width >= 992 && width < 1280) return 197
+    else if (width >= 1280 ) return 197  
   }
 }
