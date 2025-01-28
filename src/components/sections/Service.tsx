33d1cc5f8fd5c2386e1a33c9698167cd47746461
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-28 12:41:43 +0000

    Quick service fix

--- src/components/sections/Service.tsx
+++ src/components/sections/Service.tsx
@@ -13,25 +13,26 @@ const { screenWidth } = useWindowSize();
       <h2 className="text-2xl sm:text-4xl font-bold text-center leading-[43px] text-main-black mt-9">Our Services</h2>
       
       {/* Image 1 */}
-      <div className="h-[405px] md:h-[665px] relative mt-7">
+      <div className="h-[320px] md:h-[665px] relative mt-7">
         
         {/* Overlay Opacity */}
         <div className="absolute left-0 top-0 w-full h-full z-10">
-          <div className="w-1/2 md:max-w-[626px] bg-[#000000] opacity-60 h-full"></div>
+          <div className="w-[52%] md:max-w-[626px] bg-[#000000] opacity-60 h-full"></div>
         </div>
 
         {/* Text content */}
         <div className="absolute left-0 top-0 w-full h-full z-40">
-          <div className={`w-[90%] flex mx-auto ${screenWidth < 768 ? 'mt-6' : 'mt-[79px]'}  bg-red-500`}>
-            <div className="max-w-[154px] md:w-full">
+          <div className={`w-[90%] flex mx-auto ${screenWidth < 768 && 'mt-6'} md:mt-[79px]`}>
+            <div className="w-full">
               <h2 className="text-xl sm:text-[32px] leading-[39.01px] font-bold text-white">For Suppliers</h2>
-              <p className="font-normal leading-4 w-full md:w-[358px] text-sm sm:text-2xl md:leading-7 text-white mt-7">
+              <p className={`font-normal ${screenWidth < 768 && 'mt-2'} max-w-[154px] leading-4 w-full md:max-w-[358px] text-sm sm:text-2xl md:leading-7 text-white md:mt-7`}>
                 Expand Your Reach :-List your products on a trusted platform
                 where healthcare providers shop for quality and reliability.
                 Manage inventory, track sales, and grow your business all in one place
               </p>
-              <button className={`font-bold leading-[24.38px] ${screenWidth < 768 ? 'w-[136px] h-10 mt-4' : 'w-[214px] h-16 mt-[52px]'} text-base sm:text-[20px] sm:w-[304px]  rounded-lg bg-white text-blue-normal `}>
-                { screenWidth < 768 ? 'Join us' : 'Sign up as a Supplier'}
+              <button 
+                className={`font-bold leading-[24.38px] ${screenWidth < 768 && 'w-[136px] h-10 mt-4'} text-base sm:text-[20px] sm:w-[304px] rounded-lg bg-white text-blue-normal sm:h-16 sm:mt-[52px]`}>
+                <span className="block sm:hidden">Join us</span> <span className="hidden sm:block">Sign up as a Supplier</span>
               </button>
             </div>
           </div>
@@ -50,28 +51,33 @@ const { screenWidth } = useWindowSize();
       </div>
 
       {/* Image 2 */}
-      <div className="h-[665px] max-w-[100vw] relative">
-        {/* Opacity */}
+      <div className="h-[320px] md:h-[665px] relative">
+        
+        {/* Overlay Opacity */}
         <div className="absolute left-0 top-0 w-full h-full z-10">
-          <div className="max-w-[626px] bg-[#000000] opacity-60 h-full"></div>
+          <div className="w-[52%] md:max-w-[626px] bg-[#000000] opacity-60 h-full"></div>
         </div>
 
         {/* Text content */}
         <div className="absolute left-0 top-0 w-full h-full z-40">
-          <div className="w-[90%] flex mx-auto mt-[79px]">
-            <div>
-            <h2 className="text-xl sm:text-[32px] leading-[39.01px] font-bold text-white">For Buyers</h2>
-              <p className="font-normal w-full md:w-[421px] text-base sm:text-2xl leading-7 text-white mt-7">
+          <div className={`w-[90%] flex mx-auto ${screenWidth < 768 && 'mt-6'} md:mt-[79px]`}>
+            <div className="w-full">
+              <h2 className="text-xl sm:text-[32px] leading-[39.01px] font-bold text-white">For Buyers</h2>
+              <p className={`font-normal ${screenWidth < 768 && 'mt-2'} max-w-[170px] leading-4 w-full  text-sm sm:text-2xl md:max-w-[421px] md:leading-7 text-white md:mt-7`}>
                 Streamline Your Procurement :-Access verified suppliers and
                 high-quality products at competitive prices. Simplify
                 procurement processes, manage orders, and ensure on-time
                 delivery for your healthcare needs
               </p>
-              <button className="font-bold leading-[24.38px] w-[214px] text-base sm:text-[20px] sm:w-[304px] h-16 rounded-lg bg-white text-blue-normal mt-[52px]"> Sign up as a Buyer </button>
+              <button 
+                className={`font-bold leading-[24.38px] ${screenWidth < 768 && 'w-[136px] h-10 mt-4'} text-base sm:text-[20px] sm:w-[304px] rounded-lg bg-white text-blue-normal sm:h-16 sm:mt-[52px]`}>
+                <span className="block sm:hidden">Join us</span> <span className="hidden sm:block">Sign up as a Buyer</span>
+              </button>
             </div>
           </div>
         </div>
 
+        {/* Image */}
         <Image
           src={serviceImage2}
           alt="service image"
@@ -82,6 +88,7 @@ const { screenWidth } = useWindowSize();
           quality={100}
         />
       </div>
+
     </div>
   );
 }
