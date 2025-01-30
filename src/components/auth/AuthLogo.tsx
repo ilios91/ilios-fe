12ff059d8e67ce6e153978b86a3854115a91cba5
cyber9ja <ayodejioladapo15@gmail.com>
2025-01-30 13:25:59 +0000

    onboarding card

---
+++ src/components/auth/AuthLogo.tsx
@@ -0,0 +1,10 @@
+import Logo from "../icons/Logo";
+
+export default function AuthLogo() {
+  return (
+    <div className="flex flex-col gap-y-2">
+      <Logo className="w-[55.52px] h-[55.52px]"/>
+      <p className="text-bold text-[#FAFAFA] text-base leading-[18px] text-center">ILIOS</p>
+    </div>
+  );
+}
--- src/components/auth/OnboardingScreen.tsx
+++ src/components/auth/OnboardingScreen.tsx
@@ -1,8 +1,10 @@
-// import { ReactNode } from "react";
+"use client";
+
 import FacilityIcon from "../icons/FacilityIcon";
-import Logo from "../icons/Logo";
 import SupplierIcon from "../icons/SupplierIcon";
 import Button from '../common/buttons/Button';
+import OnboardingScreenCard from './OnboardingScreenCard';
+import AuthLogo from "./AuthLogo";
 
 // type UserRole = "facility" | 'supplier';
 
@@ -10,51 +12,32 @@ import Button from '../common/buttons/Button';
 function OnboardingScreen() {
 
   return (
-    <section className="h-[100vh] bg-blue-normal/70 pt-5">
+    <section className="h-[100vh] bg-blue-normal/70 pt-6">
+
+      {/* Logo */}
       <div className="flex mx-auto md:w-[90%] tablet:w-[85%]">
-        <div className="flex flex-col gap-y-2">
-          <Logo className="w-[55.52px] h-[55.52px]"/>
-          <p className="text-bold text-[#FAFAFA] text-base leading-[18px] text-center">ILIOS</p>
-        </div>
-        
+        <AuthLogo />
       </div>
+
+      {/* Content */}
       <div className="flex items-center mx-auto md:w-[90%] tablet:w-[85%] mt-10">
+
+        {/* Choose role text */}
         <div className="w-1/2 text-[#FAFAFA]">
           <h1 className="max-w-[447px] text-5xl font-bold mb-10 leading-[58px]">Choose Your Role to Get Started</h1>
           <p className="text-[32px] font-medium max-w-[400px] leading-[39px]">Select the option that best describes you!</p>
         </div>
+
+        {/* Cards */}
         <div className="flex-1 flex flex-col">
           <div className="flex gap-x-10 justify-center">
-
-            {/* Facility Card */}
-            <div className="cursor-pointer flex flex-col items-center justify-center border-[#CEDFF9] border-4 rounded-[20px] bg-white w-[299px] max-w-[299px] max-h-[424px] h-[424px]">
-              <div className="flex items-center justify-center h-[80%] w-full">
-                <FacilityIcon className="w-[275px] h-[195.25px]"/>
-              </div>
-              <div className="h-[20%]">
-                <h5 className="text-[20px] font-bold leading-[24px] text-[#171717]">Facility</h5>
-              </div>
-            </div>
-
-            {/* Supplier Card */}
-            <div className="flex flex-col items-center">
-              <div className="cursor-pointer flex flex-col items-center justify-center border-[#CEDFF9] border-4 rounded-[20px] bg-white w-[299px] max-w-[299px] max-h-[424px] h-[424px]">
-                <div className="flex items-center justify-center h-[80%] w-full">
-                  <SupplierIcon className="w-[225.55px] h-[224.8px]"/>
-                </div>
-                <div className="h-[20%]">
-                  <h5 className="text-[20px] font-bold leading-[24px] text-[#171717]">Supplier</h5>
-                </div>
-              </div>
-            </div>
+            <OnboardingScreenCard text="Facility" icon={<FacilityIcon className="w-[275px] h-[195.25px]"/>} />
+            <OnboardingScreenCard text="Supplier" icon={<SupplierIcon className="w-[225.55px] h-[224.8px]"/>} />
           </div>
-
           {/* Continue Button */}
-          <Button 
-            text="Continue" 
-            className="mt-14 mx-auto w-[518px] h-12 bg-blue-normal font-bold text-white text-[18px] rounded-lg"
-          />
+          <Button text="Continue" className="mt-14 mx-auto w-[518px] h-12 bg-blue-normal font-bold text-white text-[18px] rounded-lg"/>
         </div>
+
       </div>
     </section>
   );
